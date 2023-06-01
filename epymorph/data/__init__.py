40749e40e996a88d78d40943b6de63ff0a732102
Alexander M. Nez <alexandernez48@gmail.com>
2023-06-01 16:22:18 -0700

    Updated SIRH - AN
    added SIRH ipm and SIRH notebooks

--- epymorph/data/__init__.py
+++ epymorph/data/__init__.py
@@ -3,20 +3,21 @@ from typing import Callable
 from epymorph.data.geo.pei import load as geo_pei_load
 from epymorph.data.geo.single_pop_geo import load as single_pop_geo_load
 from epymorph.data.geo.sparsemod import load as geo_sparse_load
-from epymorph.data.geo.us_counties_2015 import \
-    load as geo_us_counties_2015_load
+from epymorph.data.geo.us_counties_2015 import load as geo_us_counties_2015_load
 from epymorph.data.geo.us_states_2015 import load as geo_us_states_2015_load
 from epymorph.data.ipm.no import load as ipm_no_load
 from epymorph.data.ipm.pei import load as ipm_pei_load
 from epymorph.data.ipm.simple_sirs import load as ipm_simple_sirs_load
+from epymorph.data.ipm.sirh import load as ipm_sirh_load
 from epymorph.movement import MovementBuilder, load_movement_spec
 
 
 def mm_loader(path) -> Callable[[], MovementBuilder]:
     def load() -> MovementBuilder:
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             spec_string = file.read()
             return load_movement_spec(spec_string)
+
     return load
 
 
@@ -24,21 +25,23 @@ def mm_loader(path) -> Callable[[], MovementBuilder]:
 # Ultimately we want to index the data directory at runtime.
 
 ipm_library = {
-    'no': ipm_no_load,
-    'pei': ipm_pei_load,
-    'simple_sirs': ipm_simple_sirs_load
+    "no": ipm_no_load,
+    "pei": ipm_pei_load,
+    "simple_sirs": ipm_simple_sirs_load,
+    "sirh": ipm_sirh_load,
 }
 
 mm_library = {
-    'no': mm_loader('epymorph/data/mm/no.movement'),
-    'icecube': mm_loader('epymorph/data/mm/icecube.movement'),
-    'pei': mm_loader('epymorph/data/mm/pei.movement')
+    "no": mm_loader("epymorph/data/mm/no.movement"),
+    "icecube": mm_loader("epymorph/data/mm/icecube.movement"),
+    "pei": mm_loader("epymorph/data/mm/pei.movement"),
+    "sparsemod": mm_loader("epymorph/data/mm/sparsemod.movement"),
 }
 
 geo_library = {
-    'pei': geo_pei_load,
-    'us_counties_2015': geo_us_counties_2015_load,
-    'us_states_2015': geo_us_states_2015_load,
-    'sparse': geo_sparse_load,
-    'single_pop_geo': single_pop_geo_load
+    "pei": geo_pei_load,
+    "us_counties_2015": geo_us_counties_2015_load,
+    "us_states_2015": geo_us_states_2015_load,
+    "sparse": geo_sparse_load,
+    "single_pop_geo": single_pop_geo_load,
 }
--- epymorph/examples/pei_py.py
+++ epymorph/examples/pei_py.py
@@ -20,45 +20,47 @@ from epymorph.util import stridesum
 #
 def ruminate(plot_results: bool, simargs: list[str]) -> None:
     # Set up the simulation...
-    geo = geo_library['pei']()
+    geo = geo_library["pei"]()
     sim = S.Simulation(
-        geo=geo,
-        ipm_builder=ipm_library['pei'](),
-        mvm_builder=load_pei_mvm()
+        geo=geo, ipm_builder=ipm_library["pei"](), mvm_builder=load_pei_mvm()
     )
 
     # ... and run it.
     t0 = time.perf_counter()
     out = sim.run(
         param={
-            'theta': 0.1,
-            'move_control': 0.9,
-            'infection_duration': np.double(4),
-            'immunity_duration': np.double(90),
-            'infection_seed_loc': 0,
-            'infection_seed_size': 10_000
+            "theta": 0.1,
+            "beta": 0.5739426782,
+            "move_control": 0.9,
+            "infection_duration": np.double(4),
+            "immunity_duration": np.double(90),
+            "infection_seed_loc": 0,
+            "infection_seed_size": 10_000,
+            "hospitalization_rate": 0.25,
+            "hospitalization_duration": 1.0,
         },
         start_date=date(2023, 1, 1),
         duration_days=150,
         # If you want consistent results, you can provide a seeded RNG.
-        rng=np.random.default_rng(1)
+        rng=np.random.default_rng(1),
     )
+
     t1 = time.perf_counter()
-    print(f"Simulation time: {(t1 - t0):.3f}s")
 
     # Output
     if plot_results:
         # Plot infections for all populations:
         event = 0
         fig, ax = plt.subplots()
-        ax.set_title('Infection incidence')
-        ax.set_xlabel('days')
+        ax.set_title("Infection incidence")
+        ax.set_xlabel("days")
         # ax.set_ylabel('infections (per 100k population)')
-        ax.set_ylabel('infections')
+        ax.set_ylabel("infections")
         x_axis = list(range(out.ctx.clock.num_days))
         for pop_idx in range(geo.nodes):
             values = stridesum(
-                out.incidence[:, pop_idx, event], out.ctx.clock.num_steps)
+                out.incidence[:, pop_idx, event], out.ctx.clock.num_steps
+            )
             # Scaled by population:
             # y_axis = values * 100_000 / geo.data['population'][pop_idx]
             # Unscaled:
@@ -71,12 +73,12 @@ def ruminate(plot_results: bool, simargs: list[str]) -> None:
         # Plot prevalence for the first population:
         pop = 0
         fig, ax = plt.subplots()
-        ax.set_title('Prevalence in FL')
-        ax.set_xlabel('days')
-        ax.set_ylabel('persons (log scale)')
-        ax.set_yscale('log')
+        ax.set_title("Prevalence in FL")
+        ax.set_xlabel("days")
+        ax.set_ylabel("persons (log scale)")
+        ax.set_yscale("log")
         x_axis = [t.tausum for t in out.ctx.clock.ticks]
-        events = ['S', 'I', 'R']
+        events = ["S", "I", "R", "H"]
         for i, event in enumerate(events):
             y_axis = out.prevalence[:, pop, i]
             ax.plot(x_axis, y_axis, label=event)
