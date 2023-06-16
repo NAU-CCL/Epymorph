ffacca6eebf920f447cd7ba1f6da8b21dc714ef5
Tyler Coles <tylercoles@gmail.com>
2023-06-16 14:04:25 -0700

    Add geo for Maricopa County by CBG in 2019.

--- epymorph/geo.py
+++ epymorph/geo.py
@@ -1,14 +1,21 @@
 from typing import NamedTuple
 
-from numpy.typing import NDArray
+import numpy as np
+from numpy.typing import DTypeLike, NDArray
 
 from epymorph.util import DataDict
 
 
-def validate_shape(name: str, data: NDArray, shape: tuple[int, ...]):
+def validate_shape(name: str, data: NDArray, shape: tuple[int, ...], dtype: DTypeLike | None = None):
     if not data.shape == shape:
         msg = f"Geo data '{name}' is incorrectly shaped; expected {shape}, loaded {data.shape}"
         raise Exception(msg)
+
+    if dtype is not None:
+        exp_dtype = np.dtype(dtype).type
+        if data.dtype.type is not exp_dtype:
+            msg = f"Geo data '{name}' is not the expected type; expected {exp_dtype.__name__}, loaded {data.dtype.type.__name__}"
+            raise Exception(msg)
     return data
 
 
