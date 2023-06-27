3e32a041acb4c9f8cef46cb1dc4a652a0a2d580a
Tyler Coles <tylercoles@gmail.com>
2023-06-26 18:04:20 -0700

    Work-around for slow type-checks.

--- epymorph/data/ipm/pei2.py
+++ epymorph/data/ipm/pei2.py
@@ -1,4 +1,4 @@
-from sympy import Expr, exp, log
+from sympy import exp, log, parse_expr
 
 from epymorph.ipm.attribute import geo, param
 from epymorph.ipm.compartment_ipm import CompartmentModelIpmBuilder
@@ -19,7 +19,12 @@ def load() -> IpmBuilder:
     [S, I, R] = symbols.compartment_symbols
     [D, L, H] = symbols.attribute_symbols
 
-    beta: Expr = (exp(-180 * H + log(2.0 - 1.3)) + 1.3) / D  # type: ignore
+    # TODO: TYPE-CHECKING THIS LINE IS SUPER SLOW
+    # see: https://github.com/microsoft/pylance-release/issues/946 (similar?)
+    # beta = (exp(-180 * H + log(2.0 - 1.3)) + 1.3) / D
+
+    # as a workaround we can just parse the expression from a string :(
+    beta = parse_expr("(exp(-180 * H + log(2.0 - 1.3)) + 1.3) / D")
 
     pei = create_model(
         symbols=symbols,
