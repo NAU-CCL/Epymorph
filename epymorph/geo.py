5690bf1c27e642de0c2a52474344db43af479350
Tyler Coles <tylercoles@gmail.com>
2023-07-14 15:28:10 -0700

    Fix bug in geo filtering: support NxN arrays.

--- epymorph/geo.py
+++ epymorph/geo.py
@@ -18,7 +18,17 @@ class Geo(NamedTuple):
 def filter_geo(geo: Geo, selection: NumpyIndices) -> Geo:
     nodes = len(selection)
     labels = (np.array(geo.labels, dtype=str)[selection]).tolist()
-    data = {key: arr[selection]
+
+    # Handle selections on attribute arrays (NxN arrays need special processing!)
+    # TODO: probably need to support TxNxN arrays too,
+    # but the relationship between time-series data and geos isn't well-founded yet
+    def select(arr: NDArray) -> NDArray:
+        if arr.shape == (geo.nodes, geo.nodes):
+            return arr[selection[:, np.newaxis], selection]
+        else:
+            return arr[selection]
+
+    data = {key: select(arr)
             for key, arr in geo.data.items()}
     return Geo(nodes, labels, data)
 
