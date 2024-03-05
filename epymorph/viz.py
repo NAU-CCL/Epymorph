4a278022cd4b839bb6267669bcd666d19adbd1c6
izaac <izaac.6.30@gmail.com>
2024-03-05 12:06:26 -0700

    improved modularity of viz module by separating out drawing and saving the model

--- epymorph/viz.py
+++ epymorph/viz.py
@@ -1,25 +1,54 @@
 from graphviz import Digraph
-from sympy import Expr, Symbol, init_printing, preview
+from sympy import Expr, Symbol, preview
 from epymorph import ipm_library
 from epymorph.compartment_model import EdgeDef, ForkDef, CompartmentModel 
 from typing import List, Union
+from IPython import display
 from tempfile import NamedTemporaryFile
-from matplotlib.image import imread
 from os import path, makedirs
-from shutil import copy
+from io import BytesIO
+from matplotlib.image import imread
 import matplotlib.pyplot as plt
 
-""" class for keeping track of the edges added to the visualization """
+class ipmDraw():
+    """class for functions related to drawing a graphviz ipm graph"""
+
+    def jupyter(graph: Digraph):
+        """draws the graph in a jupyter notebook"""
+        display.display_png(graph)
+
+    def console(graph: Digraph):
+        """draws graph to console"""
+
+        # render png of graph
+        model_bytes = graph.pipe(format='png')
+
+        # convert png bytes to bytesio object
+        model_bytes = BytesIO(model_bytes)
+
+        # read the png file for matplotlib visualization
+        ipm_png = imread(model_bytes)
+
+        # mark the model png as the plot to show
+        plt.imshow(ipm_png)
+
+        # turn of axes
+        plt.axis('off')
+
+        # show the model png
+        plt.show()       
+
 class EdgeTracker():
+    """ class for keeping track of the edges added to the visualization """
 
-    """ dictionary for tracking edges, key = (head, tail) value = edge label"""
     edge_dict = {}
+    """ dictionary for tracking edges, key = (head, tail) value = edge label"""
 
-    """ 
-    given a head, tail, and label for an edge, tracks it and updates the 
-    edge label (a sympy expr) if necessary
-    """
     def track_edge(self, head: str, tail: str, label: Expr) -> None:
+        """ 
+        given a head, tail, and label for an edge, tracks it and updates the 
+        edge label (a sympy expr) if necessary
+        """
 
         # check if edge already exists
         if (head, tail) in self.edge_dict:
@@ -34,8 +63,8 @@ class EdgeTracker():
             self.edge_dict[(head, tail)] = label
 
 
-    """ given a head and tail for an edge, return its label """
     def get_edge_label(self, head:str, tail:str) -> str:
+        """ given a head and tail for an edge, return its label """
 
         # ensure label exists
         if (head, tail) in self.edge_dict:
@@ -46,143 +75,129 @@ class EdgeTracker():
         # not in list, return empty expression
         return None
 
-    """ clears all tracked edges """
     def clear(self):
+        """ clears all tracked edges """
+
         self.edge_dict.clear()
 
 
-
-"""
-primary function for creating a model visualization, given an ipm label that 
-exists within the ipm library
-"""
-def render_model(ipm: CompartmentModel, save: bool = False, 
-                                                   filename: str = "") -> None:
-
-    # render model as a temp file
-    with NamedTemporaryFile(suffix = '.gv', delete=False) as temp_gv:
-
-        # init a tracker to be used for tacking edges and edge labels
-        tracker = EdgeTracker()
-
-        # fetch ipm transition data
-        ipm_transitions = ipm.transitions
+def build_ipm_graph(ipm: CompartmentModel) -> Digraph:
         
-        # init graph for model visualization to save to png, strict flag makes
-        # it so repeated edges are merged
-        model_viz = Digraph(format = 'png', strict=True,
-                            graph_attr = {'rankdir': 'LR'},
-                            node_attr = {'shape': 'square',
-                                         'width': '.9',
-                                         'height': '.8'},
-                            edge_attr = {'minlen': '2.0'})
+    """
+    primary function for creating a model visualization, given an ipm label 
+    that exists within the ipm library
+    """
+    # init a tracker to be used for tacking edges and edge labels
+    tracker = EdgeTracker()
 
-        # clear graph so repeated calls of render model do not repeat edges
-        model_viz.clear(keep_attrs=True)
+    # fetch ipm transition data
+    ipm_transitions = ipm.transitions
+    
+    # init graph for model visualization to save to png, strict flag makes
+    # it so repeated edges are merged
+    model_viz = Digraph(format = 'png', strict=True, 
+                        graph_attr = {'rankdir': 'LR'},
+                        node_attr = {'shape': 'square',
+                                     'width': '.9',
+                                     'height': '.8'},
+                        edge_attr = {'minlen': '2.0'})
 
-        # clear tracker, see above
-        tracker.clear()
+    # clear graph so repeated calls of render model do not repeat edges
+    model_viz.clear(keep_attrs=True)
 
+    # clear tracker, see above
+    tracker.clear()
 
-        # render edges
-        for transition in ipm_transitions:
+    # render edges
+    for transition in ipm_transitions:
 
-            # check for fork
-            if isinstance(transition, ForkDef):
+        # check for fork
+        if isinstance(transition, ForkDef):
 
-                # add fork transitions to list
-                ipm_transitions += transition.edges
+            # add fork transitions to list
+            ipm_transitions += transition.edges
 
-            # transition is an edge
-            else:
+        # transition is an edge
+        else:
 
-                # get the current head and tail of the edge
-                curr_head, curr_tail = str(transition.compartment_from), \
-                                                 str(transition.compartment_to)
-                
-                # add edge to tracker, using the rate as the label
-                tracker.track_edge(curr_head, curr_tail, transition.rate)
-
-                # get santized edge label from newly tracked edge
-                label_expr = tracker.get_edge_label(curr_head, curr_tail)
-
-                # create a temporary png file to render LaTeX edge label
-                with NamedTemporaryFile(suffix='.png', 
-                                                     delete=False) as temp_png:
-
-                    # load label as LaTeX png into temp file
-                    preview(label_expr, viewer='file', filename=temp_png.name, 
-                                                                   euler=False)
-
-                    # render edge
-                    model_viz.edge(curr_head, curr_tail, 
-                                             label=png_to_label(temp_png.name))
-
-        # render png of graph
-        model_viz.render(temp_gv.name)
-
-        # get the optimal size for the rendered graph
-        plt.figure(dpi=find_optimal_dpi(f'{temp_gv.name}.png'))
-
-
-        # show the png 
-        ipm_png = imread(f'{temp_gv.name}.png')
-        plt.imshow(ipm_png)
-        plt.axis('off')
-        plt.show()
-
-        # check if image should be saved
-        if save:
+            # get the current head and tail of the edge
+            curr_head, curr_tail = str(transition.compartment_from), \
+                                             str(transition.compartment_to)
             
-            # ensure filename not empty
-            if filename:
+            # add edge to tracker, using the rate as the label
+            tracker.track_edge(curr_head, curr_tail, transition.rate)
 
-                # create visualization directory if it doesn't exist
-                if not path.exists('model_pngs'):
+            # get santized edge label from newly tracked edge
+            label_expr = tracker.get_edge_label(curr_head, curr_tail)
 
-                    # doesn't exist, create the directory
-                    makedirs('model_pngs')
+            # create a temporary png file to render LaTeX edge label
+            with NamedTemporaryFile(suffix='.png', 
+                                                 delete=False) as temp_png:
 
-                # copy temp file to png directory to save it
-                copy(f'{temp_gv.name}.png', f'model_pngs/{filename}.png')
+                # load label as LaTeX png into temp file
+                preview(label_expr, viewer='file', filename=temp_png.name, 
+                                                               euler=False)
 
-            # file name is empty, print err message
-            else:
+                # render edge
+                model_viz.edge(curr_head, curr_tail, 
+                                         label=png_to_label(temp_png.name))
 
-                print("ERR: no file name provided, could not save model")
+    # return created visualization graph
+    return model_viz
 
+def render(ipm: CompartmentModel, save: bool = False, filename: str = "",
+                                                      console: bool = False) \
+                                                                       -> None:
+    """
+    main function for converting an ipm into a visual model
+    ipm: the model to be converted
+    save: inidicates if the model should be saved
+    filename: file name to save to, if model is to be saved
+    console: flag for printing to console vs the standard output to jupyter
+    """
+    ipm_graph = build_ipm_graph(ipm)
 
+    if console:
+        ipmDraw.console(ipm_graph)
+    
+    else:
+        ipmDraw.jupyter(ipm_graph)
+    
+    if save:
+        save_model(ipm_graph, filename)
 
-"""
-helper function for displaying an image label using graphvz, requires the image
-to be in a table
-"""
+def save_model(ipm_graph: Digraph, filename: str) -> None:
+    """
+    function that saves a given graphviz ipm digraph to a png in the
+    'model_pngs' folder with the given file name. Creates the folder if it 
+    does not exist
+    """
+
+    # ensure filename not empty
+    if filename:
+
+        # create visualization directory if it doesn't exist
+        if not path.exists('model_pngs'):
+
+            # doesn't exist, create the directory
+            makedirs('model_pngs')
+
+        # render and save png
+        ipm_graph.render(filename, 'model_pngs', cleanup=True)
+        
+
+    # file name is empty, print err message
+    else:
+        print("ERR: no file name provided, could not save model")
+
 def png_to_label(png_filepath: str) -> str:
+    """
+    helper function for displaying an image label using graphvz, requires the
+    image to be in a table
+    """
+
     return (
         f'<<TABLE border="0"><TR><TD><IMG SRC="{png_filepath}"/>' +
                                                           '</TD></TR></TABLE>>'
     )
-
-"""
-helper function for determining the best dpi (which determines size) for 
-displaying the given ipm model .png using motplotlib
-"""
-def find_optimal_dpi(ipm_png_filename: str) -> int:
-    # init dpi to start at 100
-    opt_dpi = 100
-
-    # get the size of the png file
-    png_size = path.getsize(ipm_png_filename)
-
-    # for every 10k bytes, increase the dpi
-    while (png_size > 10000):
-
-        # increase the dpi by 50
-        opt_dpi += 50
-
-        # remove 10 kbytes from the size for loop control
-        png_size -= 10000
-
-    # return the determined dpi
-    return opt_dpi
 
