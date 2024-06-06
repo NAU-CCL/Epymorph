fcf7d65360d3a93693586c74bed803a4c6208000
meaghan66 <mof25@nau.edu>
2024-06-05 20:08:53 -0700

    Addition of LODES User Manual

--- epymorph/geo/adrio/census/lodes.py
+++ epymorph/geo/adrio/census/lodes.py
@@ -7,8 +7,6 @@ from turtle import home
 from urllib.request import urlopen
 from warnings import warn
 
-import geopandas
-import matplotlib.pyplot
 import numpy as np
 import pandas as pd
 import plot_utils as pu
@@ -22,9 +20,12 @@ from epymorph.cache import (CacheMiss, CacheWarning, load_file_from_cache,
 from epymorph.data_shape import Shapes
 from epymorph.error import GeoValidationException
 from epymorph.geo.adrio.adrio import ADRIO, ADRIOMaker
-from epymorph.geo.spec import AttribDef, Geography, TimePeriod, Year
-from epymorph.geography.us_census import state_fips_to_code
+from epymorph.geo.spec import Geography, TimePeriod, Year
+from epymorph.geography.us_census import (BLOCK, BLOCK_GROUP,
+                                          CENSUS_GRANULARITY, COUNTY, STATE,
+                                          TRACT, state_fips_to_code)
 from epymorph.geography.us_tiger import _fetch_url
+from epymorph.simulation import AttributeDef, geo_attrib
 
 _LODES_CACHE_PATH = Path("geo/adrio/census/lodes")
 
@@ -69,25 +70,40 @@ class ADRIOMakerLODES(ADRIOMaker):
                         "SI03"]
 
     attributes = [
-        AttribDef('name', np.str_, Shapes.N),
-        AttribDef('home_geoid', np.str_, Shapes.N),
-        AttribDef('work_geoid', np.str_, Shapes.N),
-        AttribDef('commuters', np.int64, Shapes.NxN),
-        AttribDef('commuters_29_under', np.int64, Shapes.NxN),
-        AttribDef('commuters_30_to_54', np.int64, Shapes.NxN),
-        AttribDef('commuters_55_over', np.int64, Shapes.NxN),
-        AttribDef('commuters_1250_under_earnings', np.int64, Shapes.NxN),
-        AttribDef('commuters_1251_to_3333_earnings', np.int64, Shapes.NxN),
-        AttribDef('commuters_3333_over_earnings', np.int64, Shapes.NxN),
-        AttribDef('commuters_goods_producing_industry', np.int64, Shapes.NxN),
-        AttribDef('commuters_trade_transport_utility_industry', np.int64, Shapes.NxN),
-        AttribDef('commuters_other_industry', np.int64, Shapes.NxN),
-        AttribDef('all_jobs', np.int64, Shapes.NxN),
-        AttribDef('primary_jobs', np.int64, Shapes.NxN),
-        AttribDef('all_private_jobs', np.int64, Shapes.NxN),
-        AttribDef('private_primary_jobs', np.int64, Shapes.NxN),
-        AttribDef('all_federal_jobs', np.int64, Shapes.NxN),
-        AttribDef('federal_primary_jobs', np.int64, Shapes.NxN)
+        geo_attrib('name', dtype=str, shape=Shapes.N,
+                   comment='The proper name of the place.'),
+        geo_attrib('commuters', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters from the work geoid to the home geoid'),
+        geo_attrib('commuters_29_under', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters ages 29 and under from the work geoid to the home geoid'),
+        geo_attrib('commuters_30_to_54', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters between ages 30 to 54 from the work geoid to the home geoid'),
+        geo_attrib('commuters_55_over', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters ages 55 and over from the work geoid to the home geoid'),
+        geo_attrib('commuters_1250_under_earnings', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that earn $1250 or under monthly from the work geoid to the home geoid'),
+        geo_attrib('commuters_1251_to_3333_earnings', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that earn between $1251 to $3333 monthly from the work geoid to the home geoid'),
+        geo_attrib('commuters_3333_over_earnings', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that earn more than $3333 monthly from the work geoid to the home geoid'),
+        geo_attrib('commuters_goods_producing_industry', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in the goods producing industry from the work geoid to the home geoid'),
+        geo_attrib('commuters_trade_transport_utility_industry', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in trade, transport, or utility industries from the work geoid to the home geoid'),
+        geo_attrib('commuters_other_industry', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in any other industry than goods and producing, or trade, transport, or utilities from the work geoid to the home geoid'),
+        geo_attrib('all_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in any type of job from the work geoid to the home geoid'),
+        geo_attrib('primary_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work only in primary jobs from the work geoid to the home geoid'),
+        geo_attrib('all_private_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters, all from private jobs, from the work geoid to the home geoid'),
+        geo_attrib('private_primary_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in private primary jobs from the work geoid to the home geoid'),
+        geo_attrib('all_federal_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters, all from federal jobs, from the work geoid to the home geoid'),
+        geo_attrib('federal_primary_jobs', dtype=int, shape=Shapes.NxN,
+                   comment='The number of total commuters that work in federal primary jobs from the work geoid to the home geoid')
     ]
 
     attrib_vars = {
@@ -109,7 +125,7 @@ class ADRIOMakerLODES(ADRIOMaker):
         'federal_primary_jobs': ["JT05"]
     }
 
-    def make_adrio(self, attrib: AttribDef, geography: Geography, time_period: TimePeriod) -> ADRIO:
+    def make_adrio(self, attrib: AttributeDef, geography: Geography, time_period: TimePeriod) -> ADRIO:
         if attrib not in self.attributes:
             msg = f"{attrib.name} is not supported for the LODES data source."
             raise GeoValidationException(msg)
@@ -168,7 +184,7 @@ class ADRIOMakerLODES(ADRIOMaker):
             file_buffer.seek(0)
             return file_buffer
 
-    def sort_geoids(self, geoids: list, data_frame: DataFrame):
+    def sort_geoids(self, granularity: str, list_geoids: list, geoids: tuple, data_frame: DataFrame):
         data_frame['w_geocode'] = data_frame['w_geocode'].astype(str)
         data_frame['h_geocode'] = data_frame['h_geocode'].astype(str)
 
@@ -178,14 +194,40 @@ class ADRIOMakerLODES(ADRIOMaker):
 
             # if the file is a main file
             if w_prefix == h_prefix:
-                # if the state for the geoid matches the main file and the geoid is not in the file, remove from the list
-                geoid_to_remove = [geo for geo in geoids if geo[:2] == w_prefix and not (
-                    data_frame['w_geocode'].str.startswith(geo).any() or data_frame['h_geocode'].str.startswith(geo).any())]
+                if not geoids:
+                    print("HERE")
+                    # get the length depending on the granularity
+                    if granularity == COUNTY.name:
+                        length = COUNTY.length
 
-                for geos in geoid_to_remove:
-                    geoids.remove(geos)
+                    elif granularity == TRACT.name:
+                        length = TRACT.length
 
-        return geoids
+                    elif granularity == BLOCK.name:
+                        length = BLOCK.length
+
+                    elif granularity == BLOCK_GROUP.name:
+                        length = BLOCK_GROUP.length
+
+                    unique_geoids = data_frame['w_geocode'].apply(
+                        lambda x: x[:length]).unique()
+
+                    for geos in unique_geoids:
+                        if geos not in list_geoids:
+                            list_geoids.append(geos)
+
+                    print(list_geoids)
+
+                else:
+
+                    # if the state for the geoid matches the main file and the geoid is not in the file, remove from the list
+                    geoid_to_remove = [geo for geo in list_geoids if geo[:2] == w_prefix and not (
+                        data_frame['w_geocode'].str.startswith(geo).any() or data_frame['h_geocode'].str.startswith(geo).any())]
+
+                    for geos in geoid_to_remove:
+                        list_geoids.remove(geos)
+
+        return list_geoids
 
     # fetch files from LODES depending on the state, residence in/out of state, job type, and year
     def fetch_commuters(self, granularity: str, nodes: dict[str, list[str]], job_type: str, year: int):
@@ -258,8 +300,6 @@ class ADRIOMakerLODES(ADRIOMaker):
         # translate state FIPS code to state to use in URL
             # note, the if statement doesn't seem to be needed but gets rid of the None type error
         state_code_results = state_fips_to_code(2020)
-        if state_code_results is None:
-            print("STATE NONE")
         state_abbreviations = []
         for fips_code in state:
             state_code = state_code_results.get(fips_code).lower()
@@ -319,10 +359,12 @@ class ADRIOMakerLODES(ADRIOMaker):
                 data = pd.read_csv(file, compression="gzip", converters={
                                    'w_geocode': str, 'h_geocode': str})
 
-                list_geoids = self.sort_geoids(list_geoids, data)
-                geoid = tuple(list_geoids)
+                list_geoids = self.sort_geoids(granularity, list_geoids, geoid, data)
 
                 unfiltered_df.append(data)
+
+            # geoid = tuple(list_geoids)
+            print(list_geoids, geoid)
 
             for df in unfiltered_df:
 
@@ -330,7 +372,7 @@ class ADRIOMakerLODES(ADRIOMaker):
 
                 # filter the rows on if they start with the prefix
                 filtered_rows = df[df['h_geocode'].str.startswith(
-                    geoid) & df['w_geocode'].str.startswith(geoid)]
+                    tuple(list_geoids)) & df['w_geocode'].str.startswith(tuple(list_geoids))]
 
                 filtered_rows_list.append(filtered_rows)
 
@@ -352,7 +394,7 @@ class ADRIOMakerLODES(ADRIOMaker):
             data_frames = self.fetch_commuters(granularity, nodes, job_type, year)
 
             for df in data_frames:
-                list_geoids = self.sort_geoids(list_geoids, df)
+                list_geoids = self.sort_geoids(granularity, list_geoids, geoid, df)
 
             geoid = tuple(list_geoids)
 
@@ -378,7 +420,7 @@ class ADRIOMakerLODES(ADRIOMaker):
 
             # loop through the data frames and search for invalid geocodes
             for data_df in data_frames:
-                list_geoids = self.sort_geoids(list_geoids, data_df)
+                list_geoids = self.sort_geoids(granularity, list_geoids, geoid, data_df)
 
             geoid = tuple(list_geoids)
             n_geocode = len(geoid)
@@ -432,78 +474,53 @@ class ADRIOMakerLODES(ADRIOMaker):
         if granularity == Granularity.COUNTY.value or granularity != Granularity.STATE.value:
             county = copy(nodes.get('county'))
 
-            # check if the current value is empty
-            if county == '*':
-                msg = "Error: COUNTY value is not retrieved for county granularity"
-                raise Exception(msg)
+            # check if the current value is empty, if it is, return an empty tuple
+            if county == ['*']:
+                return ()
 
             if state and county:
                 # if there is only one state
                 home_geoids = [
                     state_code + county_code for state_code in state for county_code in county]
-                # else:
-                # aggregate based on index if there are multiple states
-                #   home_geoids = [state[i] + county[i]
-                #                 for i in range(min(len(state), len(county)))]
 
         # if the given aggregation is TRACT and that the tract value is not empty
         if granularity == Granularity.TRACT.value or granularity == Granularity.CBG.value or granularity == Granularity.BLOCK.value:
             tract = copy(nodes.get('tract'))
 
+            print("TRACT")
+
             # check if the current value is empty
-            if tract == '*':
-                # raise error
-                msg = "Error: TRACT value is not retrieved for tract granularity"
-                raise Exception(msg)
+            if tract == ['*']:
+                return ()
 
             if state and tract:
-                # if there is only one state
-                #            if len(state) == 1:
+
                 home_geoids = [
                     geoid_code + tract_code for geoid_code in home_geoids for tract_code in tract]
-      #          else:
-                # aggregate based on index if there are multiple states
-     #               home_geoids = [home_geoids[i] + tract[i]
-    #                               for i in range(min(len(home_geoids), len(tract)))]
 
         # if the given aggregation is CBG and that the CBG value is not empty
         if granularity == Granularity.CBG.value or granularity == Granularity.BLOCK.value:
             cbg = copy(nodes.get('block group'))
 
             # check if the current value is empty
-            if cbg == '*':
-                # raise error
-                msg = "Error: CBG value is not retrieved for cbg granularity"
-                raise Exception(msg)
+            if cbg == ['*']:
+                return ()
 
             if state and cbg:
-                # if there is only one state
-                #                if len(state) == 1:
+
                 home_geoids = [
                     geoid_code + cbg_code for geoid_code in home_geoids for cbg_code in cbg]
-  #              else:
-                # aggregate based on index if there are multiple states
- #                   home_geoids = [home_geoids[i] + cbg[i]
-#                                   for i in range(min(len(home_geoids), len(cbg)))]
 
         # if the given aggregation is block and the block value is not empty
         if granularity == Granularity.BLOCK.value:
             block = copy(nodes.get('block'))
 
             # check if the current value is empty
-            if block == '*':
-                # raise error
-                msg = "Error: BLOCK value is not retrieved for block granularity"
-                raise Exception(msg)
+            if block == ['*']:
+                return ()
 
             if state and block:
-                # if there is only one state
-               #             if len(state) == 1:
                 home_geoids = [
                     geoid_code + block_code for geoid_code in home_geoids for block_code in block]
-  #              else:
-                # aggregate based on index if there are multiple states
- #                   home_geoids = [home_geoids[i] + block[i]
-#                                   for i in range(min(len(home_geoids), len(block)))]
 
         return tuple(home_geoids)
