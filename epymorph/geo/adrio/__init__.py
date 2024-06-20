6a2a065d0e0c0165875fc87860af7a4d14b92885
TJohnsonAZ <tkj44@nau.edu>
2024-06-20 16:36:55 -0700

    Add HHS ADRIOMaker.

--- epymorph/geo/adrio/__init__.py
+++ epymorph/geo/adrio/__init__.py
@@ -2,10 +2,12 @@
 from epymorph.geo.adrio.census.adrio_census import ADRIOMakerCensus
 from epymorph.geo.adrio.census.lodes import ADRIOMakerLODES
 from epymorph.geo.adrio.file.adrio_csv import ADRIOMakerCSV
+from epymorph.geo.adrio.hhs.adrio_hhs import ADRIOMakerHHS
 from epymorph.geo.dynamic import ADRIOMaker
 
 adrio_maker_library: dict[str, type[ADRIOMaker]] = {
     'Census': ADRIOMakerCensus,
     'CSV': ADRIOMakerCSV,
     'LODES': ADRIOMakerLODES,
+    'HHS': ADRIOMakerHHS
 }
