d44554004b3d5e8217966f2465271626c0094653
TJohnsonAZ <tkj44@nau.edu>
2024-07-08 18:02:00 -0700

    Date filtering and devlog improvements.

--- epymorph/geo/adrio/cdc/adrio_cdc.py
+++ epymorph/geo/adrio/cdc/adrio_cdc.py
@@ -234,21 +234,15 @@ class ADRIOMakerCDC(ADRIOMaker):
 
         def fetch() -> NDArray:
             if time_period.end_date >= date(2024, 5, 1):
-                print("State level hospitalization data is voluntary past 5/1/2024.")
+                warn("State level hospitalization data is voluntary past 5/1/2024.")
 
             state_mapping = state_fips_to_code(scope.year)
             fips = scope.get_node_ids()
-            state_codes = '\'' + '\',\''.join([state_mapping[x] for x in fips]) + '\''
+            state_codes = ",".join(f"'{state_mapping[x]}'" for x in fips)
             col_name = self.attribute_cols[attrib.name]
 
-            url = f"https://data.cdc.gov/resource/aemt-mg7g.csv?$select=week_end_date,jurisdiction,{col_name}&$where=jurisdiction%20in({state_codes})&$limit=11514"
+            url = f"https://data.cdc.gov/resource/aemt-mg7g.csv?$select=week_end_date,jurisdiction,{col_name}&$where=jurisdiction%20in({state_codes})%20AND%20week_end_date%20between%20'{time_period.start_date}T00:00:00'%20and%20'{time_period.end_date}T00:00:00'&$limit=11514"
             df = read_csv(url)
-
-            df['week_end_date'] = [datetime.fromisoformat(
-                week).date() for week in df['week_end_date']]
-
-            df = df[df['week_end_date'] >= time_period.start_date]
-            df = df[df['week_end_date'] < time_period.end_date]
 
             df = df.groupby(['week_end_date', 'jurisdiction']).sum()
             df.reset_index(inplace=True)
