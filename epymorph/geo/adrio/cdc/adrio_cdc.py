cf0230c53625aa517c31087de5ffcc5190327d79
TJohnsonAZ <tkj44@nau.edu>
2024-07-13 15:42:40 -0700

    Combine query cases, add paging, return date string.

--- epymorph/geo/adrio/cdc/adrio_cdc.py
+++ epymorph/geo/adrio/cdc/adrio_cdc.py
@@ -241,21 +241,16 @@ class ADRIOMakerCDC(ADRIOMaker):
 
             state_mapping = state_fips_to_code(scope.year)
             fips = scope.get_node_ids()
-            # state_codes = ",".join(f"'{state_mapping[x]}'" for x in fips)
             state_codes = np.array([state_mapping[x] for x in fips])
-            # col_name = self.attribute_cols[attrib.name]
 
             df = self._api_query(info, state_codes, time_period, scope.granularity)
-
-            # url = f"https://data.cdc.gov/resource/aemt-mg7g.csv?$select=week_end_date,jurisdiction,{col_name}&$where=jurisdiction%20in({state_codes})%20AND%20week_end_date%20between%20'{time_period.start_date}T00:00:00'%20and%20'{time_period.end_date}T00:00:00'&$limit=11514"
-            # df = read_csv(url)
 
             df = df.groupby(['week_end_date', 'jurisdiction']).sum()
             df.reset_index(inplace=True)
             df = df.pivot(index='week_end_date',
                           columns='jurisdiction', values=info.data_col)
 
-            return df.to_numpy(dtype=attrib.dtype)
+            return np.array(list(zip(df.index.values, df.to_numpy(dtype=attrib.dtype))), dtype=[('date', object), ('data', object)])
 
         return ADRIO(attrib.name, fetch)
 
