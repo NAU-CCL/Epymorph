d4900f49126cd48cf682bca2a1fc96a6bd10aa8f
TJohnsonAZ <72234106+TJohnsonAZ@users.noreply.github.com>
2024-08-15 11:32:23 -0700

    Adrio2 cdc devlog (#143)
    Update cdc devlog for adrio2 refactor.

--- epymorph/geo/adrio/__init__.py
+++ epymorph/geo/adrio/__init__.py
@@ -1,7 +1,7 @@
 """AdrioMaker library."""
-from epymorph.geo.adrio.cdc.adrio_cdc import ADRIOMakerCDC
 from epymorph.geo.adrio.census.adrio_census import ADRIOMakerCensus
 from epymorph.geo.adrio.census.lodes import ADRIOMakerLODES
+from epymorph.geo.adrio.disease.adrio_cdc import ADRIOMakerCDC
 from epymorph.geo.adrio.file.adrio_csv import ADRIOMakerCSV
 from epymorph.geo.dynamic import ADRIOMaker
 
