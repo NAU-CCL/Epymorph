daf969595762dfd79423000526fb0bd9ae3e328d
TJohnsonAZ <tkj44@nau.edu>
2024-08-28 13:38:40 -0700

    Rename variables for consistency.

--- epymorph/adrio/numpy.py
+++ epymorph/adrio/numpy.py
@@ -13,26 +13,29 @@ class NPY(Adrio[Any]):
 
     file_path: PathLike
     """The path to the .npy file containing data."""
-    arr_slice: list[slice] | None
+    array_slice: list[slice] | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, slice: list[slice] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_slice: list[slice] | None = None) -> None:
         self.file_path = file_path
-        self.slice = slice
+        self.array_slice = array_slice
 
     def evaluate(self) -> NDArray:
         data = np.load(self.file_path)
 
         data = np.array(data)
 
-        if self.arr_slice is not None:
-            if len(self.arr_slice) != data.ndim:
+        if self.array_slice is not None:
+            if len(self.array_slice) != data.ndim:
                 msg = 'One slice is required for each array axis.'
                 raise DataResourceException(msg)
             axis = 0
-            for x in self.arr_slice:
-                if x is not None and isinstance(x, slice):
-                    data = data.take(indices=range(x.start, x.stop), axis=axis)
+            for curr_slice in self.array_slice:
+                if curr_slice is not None and isinstance(curr_slice, slice):
+                    data = data.take(
+                        indices=range(curr_slice.start, curr_slice.stop),
+                        axis=axis
+                    )
                 axis += 1
 
         return data
@@ -45,26 +48,29 @@ class NPZ(Adrio[Any]):
     """The path to the .npz file containing data."""
     array_name: str
     """The name of the array in the .npz file to load."""
-    arr_slice: list[slice] | None
+    array_slice: list[slice] | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, array_name: str, arr_slice: list[slice] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_name: str, array_slice: list[slice] | None = None) -> None:
         self.file_path = file_path
         self.array_name = array_name
-        self.arr_slice = arr_slice
+        self.array_slice = array_slice
 
     def evaluate(self) -> NDArray:
         data = np.load(self.file_path)
         data = np.array(data[self.array_name])
 
-        if self.arr_slice is not None:
-            if len(self.arr_slice) != data.ndim:
+        if self.array_slice is not None:
+            if len(self.array_slice) != data.ndim:
                 msg = 'One slice is required for each array axis.'
                 raise DataResourceException(msg)
             axis = 0
-            for x in self.arr_slice:
-                if x is not None and isinstance(x, slice):
-                    data = data.take(indices=range(x.start, x.stop), axis=axis)
+            for curr_slice in self.array_slice:
+                if curr_slice is not None and isinstance(curr_slice, slice):
+                    data = data.take(
+                        indices=range(curr_slice.start, curr_slice.stop),
+                        axis=axis
+                    )
                 axis += 1
 
         return data
