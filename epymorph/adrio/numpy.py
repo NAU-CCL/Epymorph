d3b73de4c791bc1f3d1db996d2169cc8eddbe6f5
TJohnsonAZ <tkj44@nau.edu>
2024-08-29 12:59:08 -0700

    Typing fixes and cleanup.

--- epymorph/adrio/numpy.py
+++ epymorph/adrio/numpy.py
@@ -1,8 +1,9 @@
 from os import PathLike
-from typing import Any
+from typing import Any, Sequence, cast
 
 import numpy as np
 from numpy.typing import NDArray
+from typing_extensions import override
 
 from epymorph.adrio.adrio import Adrio
 from epymorph.error import DataResourceException
@@ -13,17 +14,17 @@ class NPY(Adrio[Any]):
 
     file_path: PathLike
     """The path to the .npy file containing data."""
-    array_slice: list[slice] | None
+    array_slice: Sequence[slice] | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, array_slice: list[slice] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_slice: Sequence[slice] | None = None) -> None:
         self.file_path = file_path
         self.array_slice = array_slice
 
+    @override
     def evaluate(self) -> NDArray:
         try:
-            data = np.load(self.file_path)
-            data = np.array(data)
+            data = cast(NDArray, np.load(self.file_path))
         except OSError as e:
             msg = 'File not found.'
             raise DataResourceException(msg) from e
@@ -35,13 +36,12 @@ class NPY(Adrio[Any]):
             if len(self.array_slice) != data.ndim:
                 msg = 'One slice is required for each array axis.'
                 raise DataResourceException(msg)
-            axis = 0
-            for curr_slice in self.array_slice:
+
+            for axis, curr_slice in enumerate(self.array_slice):
                 data = data.take(
                     indices=range(curr_slice.start, curr_slice.stop),
                     axis=axis
                 )
-                axis += 1
 
         return data
 
@@ -53,18 +53,17 @@ class NPZ(Adrio[Any]):
     """The path to the .npz file containing data."""
     array_name: str
     """The name of the array in the .npz file to load."""
-    array_slice: list[slice] | None
+    array_slice: Sequence[slice] | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, array_name: str, array_slice: list[slice] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_name: str, array_slice: Sequence[slice] | None = None) -> None:
         self.file_path = file_path
         self.array_name = array_name
         self.array_slice = array_slice
 
     def evaluate(self) -> NDArray:
         try:
-            data = np.load(self.file_path)
-            data = np.array(data[self.array_name])
+            data = cast(NDArray, np.load(self.file_path))
         except OSError as e:
             msg = 'File not found.'
             raise DataResourceException(msg) from e
@@ -76,12 +75,11 @@ class NPZ(Adrio[Any]):
             if len(self.array_slice) != data.ndim:
                 msg = 'One slice is required for each array axis.'
                 raise DataResourceException(msg)
-            axis = 0
-            for curr_slice in self.array_slice:
+
+            for axis, curr_slice in enumerate(self.array_slice):
                 data = data.take(
                     indices=range(curr_slice.start, curr_slice.stop),
                     axis=axis
                 )
-                axis += 1
 
         return data
