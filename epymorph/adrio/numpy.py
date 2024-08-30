6c5eb68fc2f2f0092314217ac5dd662dc52c429a
Tyler Coles <tylercoles@gmail.com>
2024-08-30 10:14:56 -0700

    Support any valid slice, including use of np.s_ and np.index_exp.

--- epymorph/adrio/numpy.py
+++ epymorph/adrio/numpy.py
@@ -9,42 +9,41 @@ from typing_extensions import override
 from epymorph.adrio.adrio import Adrio
 from epymorph.error import DataResourceException
 
+_SliceLike = slice | type(Ellipsis)
+_ArraySlice = _SliceLike | tuple[_SliceLike, ...]
+
 
 class NPY(Adrio[Any]):
     """Retrieves an array of data from a user-provided .npy file."""
 
     file_path: PathLike
     """The path to the .npy file containing data."""
-    array_slice: tuple[slice, ...] | None
+    array_slice: _ArraySlice | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, array_slice: tuple[slice, ...] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_slice: _ArraySlice | None = None) -> None:
+        if Path(file_path).suffix != '.npy':
+            msg = 'Incorrect file type. Only .npy files can be loaded through NPY ADRIOs.'
+            raise DataResourceException(msg)
         self.file_path = file_path
         self.array_slice = array_slice
 
     @override
     def evaluate(self) -> NDArray:
-        if Path(self.file_path).suffix != '.npy':
-            msg = 'Incorrect file type. Only .npy files can be loaded through NPY ADRIOs.'
-            raise DataResourceException(msg)
-
         try:
             data = cast(NDArray, np.load(self.file_path))
+            if self.array_slice is not None:
+                data = data[self.array_slice]
+            return data
         except OSError as e:
             msg = 'File not found.'
             raise DataResourceException(msg) from e
         except ValueError as e:
             msg = 'Object arrays cannot be loaded.'
             raise DataResourceException(msg) from e
-
-        if self.array_slice is not None and len(self.array_slice) != 0:
-            if len(self.array_slice) != data.ndim:
-                msg = 'One slice is required for each array axis.'
-                raise DataResourceException(msg)
-
-            data = data[self.array_slice]
-
-        return data
+        except IndexError as e:
+            msg = 'Specified array slice is invalid for the shape of this data.'
+            raise DataResourceException(msg) from e
 
 
 class NPZ(Adrio[Any]):
@@ -54,33 +53,30 @@ class NPZ(Adrio[Any]):
     """The path to the .npz file containing data."""
     array_name: str
     """The name of the array in the .npz file to load."""
-    array_slice: tuple[slice, ...] | None
+    array_slice: _ArraySlice | None
     """Optional slice(s) of the array to load."""
 
-    def __init__(self, file_path: PathLike, array_name: str, array_slice: tuple[slice, ...] | None = None) -> None:
+    def __init__(self, file_path: PathLike, array_name: str, array_slice: _ArraySlice | None = None) -> None:
+        if Path(file_path).suffix != '.npz':
+            msg = 'Incorrect file type. Only .npz files can be loaded through NPZ ADRIOs.'
+            raise DataResourceException(msg)
         self.file_path = file_path
         self.array_name = array_name
         self.array_slice = array_slice
 
+    @override
     def evaluate(self) -> NDArray:
-        if Path(self.file_path).suffix != '.npz':
-            msg = 'Incorrect file type. Only .npz files can be loaded through NPZ ADRIOs.'
-            raise DataResourceException(msg)
-
         try:
             data = cast(NDArray, np.load(self.file_path)[self.array_name])
+            if self.array_slice is not None:
+                data = data[self.array_slice]
+            return data
         except OSError as e:
             msg = 'File not found.'
             raise DataResourceException(msg) from e
         except ValueError as e:
             msg = 'Object arrays cannot be loaded.'
             raise DataResourceException(msg) from e
-
-        if self.array_slice is not None and len(self.array_slice) != 0:
-            if len(self.array_slice) != data.ndim:
-                msg = 'One slice is required for each array axis.'
-                raise DataResourceException(msg)
-
-            data = data[self.array_slice]
-
-        return data
+        except IndexError as e:
+            msg = 'Specified array slice is invalid for the shape of this data.'
+            raise DataResourceException(msg) from e
