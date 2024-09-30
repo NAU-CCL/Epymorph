80090a2f9ebc1f679a826f3510d92063f36967f2
meaghan66 <102330088+meaghan66@users.noreply.github.com>
2024-09-30 13:12:31 -0700

    Humidity ADRIO (#167)
    * Humidity ADRIO template and basic demo devlog.
    
    * Modified README.
    
    * Providing sources to equations.
    
    * Consistent constants among equations.
    
    * Added warning for relative humidity exceeding 100%

---
+++ epymorph/adrio/humidity.py
@@ -0,0 +1,78 @@
+from warnings import warn
+
+import numpy as np
+from numpy.typing import NDArray
+from typing_extensions import override
+
+from epymorph.adrio.adrio import Adrio
+from epymorph.data_shape import Shapes
+from epymorph.simulation import AttributeDef
+
+
+class AbsoluteHumidity(Adrio[np.float64]):
+    """
+    Creates a TxN matrix of floats representing absolute humidity in kilograms per cubic
+    meter calculated from a relative humidity, which is calculated from a given
+    temperature and dew point temperature, both in degrees Celsius.
+    """
+
+    requirements = [
+        AttributeDef("temperature", type=float, shape=Shapes.TxN),
+        AttributeDef("dewpoint", type=float, shape=Shapes.TxN),
+    ]
+
+    @override
+    def evaluate_adrio(self) -> NDArray[np.float64]:
+        temperature = self.data("temperature")
+        relH = self.defer(RelativeHumidity())
+        npHumidity = []
+
+        # equation from relative humidity to absolute humidity provided by following url
+        # https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
+        # values 17.67 and 243.5 are changed to 17.625 and 243.04 respectively to cover
+        # a larger range of temperature values with a smaller margin of error
+        # (Alduchov and Eskridge 1996)
+        constants = 6.112 * 2.16679
+        npHumidity = (
+            (
+                constants
+                * np.exp((17.625 * temperature) / (temperature + 243.04))
+                * (relH)
+            )
+            / (273.15 + temperature)
+            / 1000  # convert to kilograms
+        )
+
+        return npHumidity
+
+
+class RelativeHumidity(Adrio[np.float64]):
+    """
+    Creates a TxN matrix of floats representing relative humidity as a percentage
+    which is calculated from a given temperature and dew point temperature, both in
+    degrees Celsius.
+    """
+
+    requirements = [
+        AttributeDef("temperature", type=float, shape=Shapes.TxN),
+        AttributeDef("dewpoint", type=float, shape=Shapes.TxN),
+    ]
+
+    @override
+    def evaluate_adrio(self) -> NDArray[np.float64]:
+        temperature = self.data("temperature")
+        dewpoint = self.data("dewpoint")
+        npHumidity = []
+
+        # equation for calculating relative humidity provided by following url
+        # https://qed.epa.gov/hms/meteorology/humidity/algorithms/#:~:text=Relative%20humidity%20is%20calculated%20using,is%20air%20temperature%20(celsius).
+        npHumidity = 100 * (
+            np.exp((17.625 * dewpoint) / (243.04 + dewpoint))
+            / np.exp((17.625 * temperature) / (243.04 + temperature))
+        )
+
+        # if the humidity exceeds 100%, warn users
+        if (npHumidity > 100).any():
+            warn("At least one value of relative humidity exceeds 100%.")
+
+        return np.array(npHumidity, dtype=np.float64)
