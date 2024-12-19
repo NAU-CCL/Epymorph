0cca9569ec9c6eb1d242ceb652028ca675227578
Tyler Coles <tylercoles@gmail.com>
2024-12-18 18:28:10 -0700

    Fix a spurious warning about the No IPM.

--- epymorph/data/ipm/no.py
+++ epymorph/data/ipm/no.py
@@ -6,6 +6,8 @@ from epymorph.compartment_model import CompartmentModel, compartment
 class No(CompartmentModel):
     """The 'no' IPM: a single compartment with no transitions."""
 
+    _suppress_ipm_validation_warnings = True
+
     compartments = (compartment("P"),)
 
     def edges(self, symbols):
