77fec0da92b2406ba71ffd7a28d1d7d03bc8be79
Tyler Coles <tylercoles@gmail.com>
2025-04-14 14:30:12 -0700

    Repair respiratory_cdc ADRIOs by restoring needed old-code.

--- epymorph/adrio/respiratory_cdc.py
+++ epymorph/adrio/respiratory_cdc.py
@@ -1,17 +1,159 @@
+from dataclasses import dataclass
 from datetime import date
-from typing import Literal
+from typing import Literal, Mapping
+from urllib.parse import quote, urlencode
 from warnings import warn
 
 import numpy as np
+import pandas as pd
 from numpy.typing import NDArray
 from typing_extensions import override
 
 from epymorph.adrio.adrio import ADRIOLegacy, ProgressCallback, adrio_legacy_cache
-from epymorph.adrio.cdc import DataSource, _api_query, _validate_scope
 from epymorph.error import DataResourceError
+from epymorph.geography.scope import GeoScope
 from epymorph.geography.us_census import CensusScope
+from epymorph.geography.us_geography import STATE, CensusGranularityName
 from epymorph.geography.us_tiger import get_states
 from epymorph.time import TimeFrame
+
+
+@dataclass(frozen=True)
+class DataSource:
+    url_base: str
+    date_col: str
+    fips_col: str
+    data_col: str
+    granularity: CensusGranularityName
+    """The geographic granularity of the source data."""
+    replace_sentinel: float | None
+    """If None, ignore sentinel values (-999999); otherwise, replace them with
+    the given value."""
+    map_geo_ids: Mapping[str, str] | None = None
+    """If None, use the scope node IDs as they are, otherwise use this mapping
+    to map them."""
+
+
+def _query_location(
+    info: DataSource, loc_clause: str, date_clause: str
+) -> pd.DataFrame:
+    """
+    Helper function for _api_query() that builds and sends queries for
+    individual locations.
+    """
+    current_return = 10000
+    total_returned = 0
+    cdc_df = pd.DataFrame()
+
+    group_col = ""
+    group_filter = ""
+    group_filter = (
+        "AND `group`='By Week'"
+        if info.url_base == "https://data.cdc.gov/resource/r8kw-7aab.csv?"
+        else ""
+    )
+
+    while current_return == 10000:
+        url = info.url_base + urlencode(
+            quote_via=quote,
+            safe=",()'$:",
+            query={
+                "$select": (
+                    f"{info.date_col},{info.fips_col},{info.data_col}{group_col}"
+                ),
+                "$where": f"{loc_clause} AND {date_clause}{group_filter}",
+                "$limit": 10000,
+                "$offset": total_returned,
+            },
+        )
+
+        cdc_df = pd.concat([cdc_df, pd.read_csv(url, dtype={info.fips_col: str})])
+
+        current_return = len(cdc_df.index) - total_returned
+        total_returned += current_return
+
+    return cdc_df
+
+
+_SENTINEL = -999999
+"""A common sentinel value which represents values which have been redacted
+for privacy because there were less than 4 individuals in that data point."""
+
+
+def _api_query(
+    source: DataSource,
+    scope: CensusScope,
+    time_frame: TimeFrame,
+    progress: ProgressCallback,
+) -> NDArray[np.float64]:
+    """
+    Composes URLs to query API and sends query requests. Limits each query to
+    10000 rows, combining several query results if this number is exceeded.
+    Returns Dataframe containing requested data sorted by date and location fips.
+    """
+    node_ids = (
+        [source.map_geo_ids[x] for x in scope.node_ids]
+        if source.map_geo_ids is not None
+        else scope.node_ids
+    )
+    if scope.granularity == "state" and source.granularity != "state":
+        # query county level data with state fips codes
+        location_clauses = [f"starts_with({source.fips_col}, '{x}')" for x in node_ids]
+    else:
+        # query with full fips codes
+        formatted_fips = ",".join(f"'{node}'" for node in node_ids)
+        location_clauses = [f"{source.fips_col} in ({formatted_fips})"]
+
+    date_clause = (
+        f"{source.date_col} "
+        f"between '{time_frame.start_date}T00:00:00' "
+        f"and '{time_frame.end_date}T00:00:00'"
+    )
+
+    processing_steps = len(location_clauses) + 1
+
+    def query_step(index, loc_clause) -> pd.DataFrame:
+        step_result = _query_location(source, loc_clause, date_clause)
+        progress((index + 1) / processing_steps, None)
+        return step_result
+
+    cdc_df = pd.concat(
+        [query_step(i, loc_clause) for i, loc_clause in enumerate(location_clauses)]
+    )
+
+    if source.replace_sentinel is not None:
+        num_sentinel = (cdc_df[source.data_col] == _SENTINEL).sum()
+        if num_sentinel > 0:
+            cdc_df = cdc_df.replace(_SENTINEL, source.replace_sentinel)
+            warn(
+                f"{num_sentinel} values < 4 were replaced with "
+                f"{source.replace_sentinel} in returned data."
+            )
+
+    if scope.granularity == "state" and source.granularity != "state":
+        # aggregate county data to state level
+        cdc_df[source.fips_col] = cdc_df[source.fips_col].map(STATE.truncate)
+        cdc_df = cdc_df.groupby([source.fips_col, source.date_col]).sum().reset_index()
+
+    return _as_numpy(
+        cdc_df.sort_values(by=[source.date_col, source.fips_col]).pivot_table(
+            index=source.date_col,
+            columns=source.fips_col,
+            values=source.data_col,
+        )
+    )
+
+
+def _as_numpy(data_df: pd.DataFrame) -> NDArray[np.float64]:
+    """Convert a DataFrame to a time-series by node numpy array where each value is a
+    tuple of date and data value. Note: this time-series is not necessarily the same
+    length as simulation T, because not all ADRIOs produce a daily value."""
+    dates = data_df.index.to_numpy(dtype="datetime64[D]")
+    return np.array(
+        [list(zip(dates, data_df[col], strict=True)) for col in data_df.columns],
+        dtype=[("date", "datetime64[D]"), ("data", np.float64)],
+    ).T
+
 
 DiseaseType = Literal["Covid", "Influenza", "RSV"]
 
@@ -115,6 +257,13 @@ class _RespiratoryADRIO(ADRIOLegacy[np.float64]):
             warn(msg)
 
         return self.data_time_frame
+
+
+def _validate_scope(scope: GeoScope) -> CensusScope:
+    if not isinstance(scope, CensusScope):
+        msg = "Census scope is required for CDC attributes."
+        raise DataResourceError(msg)
+    return scope
 
 
 class DiseaseHospitalizations(_RespiratoryADRIO):
