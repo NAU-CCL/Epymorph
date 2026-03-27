4204b036f6112bed1369b2dd5bcab728f1160e99
averydx <avery.drennan@gmail.com>
2026-03-27 10:17:38 -0700

    cleanup in parameter selection.

--- epymorph/time.py
+++ epymorph/time.py
@@ -733,11 +733,13 @@ class TimeAggMethod(NamedTuple):
     """The method for aggregating compartment values."""
     events: AggMethod
     """The method for aggregating event values."""
+    parameters: AggMethod
+    """The method for aggregating unknown parameter values."""
 
 
-DEFAULT_TIME_AGG = TimeAggMethod("last", "sum")
-"""By default, time series should be aggregated using 'last' for compartments
-and 'sum' for events."""
+DEFAULT_TIME_AGG = TimeAggMethod("last", "sum", "last")
+"""By default, time series should be aggregated using 'last'
+for compartments and parameters and 'sum' for events."""
 
 
 @dataclass(frozen=True)
@@ -859,6 +861,7 @@ class _CanAggregate(TimeStrategy):
         self,
         compartments: AggMethod = "last",
         events: AggMethod = "sum",
+        parameters: AggMethod = "last"
     ) -> "TimeAggregation":
         """
         Aggregate the time series using the specified methods.
@@ -879,7 +882,7 @@ class _CanAggregate(TimeStrategy):
             self.time_frame,
             self.selection,
             self.grouping,
-            TimeAggMethod(compartments, events),
+            TimeAggMethod(compartments, events,parameters),
         )
 
 
